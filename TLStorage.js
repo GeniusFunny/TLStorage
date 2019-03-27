7707b18af85a9c599c7c09499f7b52f8a913e213
Genius Funny <geniusfunnyyh@gmail.com>
2019-03-27 16:07:13 +0800

    代码优化、注释完善

--- TLStorage.js
+++ TLStorage.js
@@ -1,43 +1,93 @@
-class TLStorage {
-  // 存储数据，time为数据有效时间，默认为1天
-  static setItem(key, value, time) {
-    time = time > 0 ? time * 1000 : 24 * 3600 * 1000
-    let wrapItem = TLStorage.wrapValue(value, time) //  包装数据项
-    localStorage.setItem(key, JSON.stringify(wrapItem)) // 将包装后的数据项存入storage
-    TLStorage.autoDelete(key, time) //  触发一个自动删除的timeout，适用于超短有效期并且标签页可能会关闭的情况下
+/**
+ *
+ * @param name，属性名称
+ * @param value，属性值
+ * @param type，属性类型
+ */
+function isCorrectType(name, value, type) {
+  if (typeof value !== type && (type === 'array' && !Array.isArray(value))) {
+    throw new Error(`${name} is must be ${type}`)
   }
-  //  定时删除函数
+}
+/**
+ * TLStorge, 有效期版LocalStorage
+ */
+class TLStorage {
+  /**
+   * 存储数据
+   * @param key
+   * @param value
+   * @param time，过期时间
+   */
+  static setItem(key, value, time) {
+    try {
+      isCorrectType('key', key, 'string')
+      isCorrectType('time', time, 'number')
+    } catch (e) {
+      throw new Error(`参数错误, ${e}`)
+    }
+    time = time > 0 ? time * 1000 : 24 * 3600 * 1000
+    const wrapItem = TLStorage.wrapValue(value, time)
+    localStorage.setItem(key, JSON.stringify(wrapItem))
+    // 触发一个自动删除的timeout，适用于有效期较短并且标签页可能会关闭的情况下
+    TLStorage.autoDelete(key, time)
+  }
+
+  /**
+   * 定时删除数据
+   * @param key
+   * @param time
+   */
   static autoDelete(key, time) {
     setTimeout(() => {
       localStorage.removeItem(key)
     }, time)
   }
-  //  包装函数，待优化
+
+  /**
+   * 包装数据项，增加过期时间
+   * @param value
+   * @param time
+   * @returns {{expireTime: *, value: *}}
+   */
   static wrapValue(value, time) {
     return {
       value: value,
       expireTime: Date.now() + time
     }
   }
-  //  获取数据；先判断是否过期，过期就删除
+
+  /**
+   * 获取数据项，在这里进行数据过滤
+   * @param key
+   * @returns {*}
+   */
   static getItem(key) {
-    let now = Date.now()
+    let now = Date.now() // 读取时间
     let wrapItem = localStorage.getItem(key)
     if (wrapItem) {
       wrapItem = JSON.parse(wrapItem)
     }
-    if (parseInt(wrapItem.expireTime) < now) {
+    // 读取时间与过期时间进行比较，如果过期则返回null，否则返回value
+    if (parseInt(wrapItem.expireTime) <= now) {
       TLStorage.removeItem(key)
       return null
     } else {
       return wrapItem.value
     }
   }
-  //  清除所有的数据
+
+  /**
+   * 清除所有数据项
+   */
   static clear() {
     localStorage.clear()
   }
-  //  删除指定数据
+
+  /**
+   * 删除数据项
+   * @param key
+   */
   static removeItem(key) {
     localStorage.removeItem(key)
   }
--- TLStorage_es5.js
+++ TLStorage_es5.js
@@ -1,79 +1,88 @@
-"use strict";
-
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
-
-function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var TLStorage =
-  /*#__PURE__*/
-  function () {
-    function TLStorage() {
-      _classCallCheck(this, TLStorage);
-    }
-
-    _createClass(TLStorage, null, [{
-      key: "setItem",
-      // 存储数据，time为数据有效时间，默认为1天
-      value: function setItem(key, value, time) {
-        time = time > 0 ? time * 1000 : 24 * 3600 * 1000;
-        var wrapItem = TLStorage.wrapValue(value, time); //  包装数据项
-
-        localStorage.setItem(key, JSON.stringify(wrapItem)); // 将包装后的数据项存入storage
-
-        TLStorage.autoDelete(key, time); //  触发一个自动删除的timeout，适用于超短有效期并且标签页可能会关闭的情况下
-      } //  定时删除函数
-
-    }, {
-      key: "autoDelete",
-      value: function autoDelete(key, time) {
-        setTimeout(function () {
-          localStorage.removeItem(key);
-        }, time);
-      } //  包装函数，待优化
-
-    }, {
-      key: "wrapValue",
-      value: function wrapValue(value, time) {
-        return {
-          value: value,
-          expireTime: Date.now() + time
-        };
-      } //  获取数据；先判断是否过期，过期就删除
-
-    }, {
-      key: "getItem",
-      value: function getItem(key) {
-        var now = Date.now();
-        var wrapItem = localStorage.getItem(key);
-
-        if (wrapItem) {
-          wrapItem = JSON.parse(wrapItem);
-        }
-
-        if (parseInt(wrapItem.expireTime) < now) {
-          TLStorage.removeItem(key);
-          return null;
-        } else {
-          return wrapItem.value;
-        }
-      } //  清除所有的数据
-
-    }, {
-      key: "clear",
-      value: function clear() {
-        localStorage.clear();
-      } //  删除指定数据
-
-    }, {
-      key: "removeItem",
-      value: function removeItem(key) {
-        localStorage.removeItem(key);
-      }
-    }]);
-
-    return TLStorage;
-  }();
+/**
+ *
+ * @param name，属性名称
+ * @param value，属性值
+ * @param type，属性类型
+ */
+function isCorrectType(name, value, type) {
+  if (typeof value !== type && (type === 'array' && !Array.isArray(value))) {
+    throw new Error(`${name} is must be ${type}`)
+  }
+}
+/**
+ * TLStorge, 有效期版LocalStorage
+ */
+function TLStorage() {}
+/**
+ * 存储数据
+ * @param key
+ * @param value
+ * @param time，过期时间
+ */
+TLStorage.setItem = function (key, value, time) {
+  try {
+    isCorrectType('key', key, 'string')
+    isCorrectType('time', time, 'number')
+  } catch (e) {
+    throw new Error(`参数错误, ${e}`)
+  }
+  time = time > 0 ? time * 1000 : 24 * 3600 * 1000
+  const wrapItem = TLStorage.wrapValue(value, time)
+  localStorage.setItem(key, JSON.stringify(wrapItem))
+  // 触发一个自动删除的timeout，适用于有效期较短并且标签页可能会关闭的情况下
+  TLStorage.autoDelete(key, time)
+}
+/**
+ * 定时删除数据
+ * @param key
+ * @param time
+ */
+TLStorage.autoDelete = function (key, time) {
+  setTimeout(() => {
+    localStorage.removeItem(key)
+  }, time)
+}
+/**
+ * 包装数据项，增加过期时间
+ * @param value
+ * @param time
+ * @returns {{expireTime: *, value: *}}
+ */
+TLStorage.wrapValue = function(value, time) {
+  return {
+    value: value,
+    expireTime: Date.now() + time
+  }
+}
+/**
+ * 获取数据项，在这里进行数据过滤
+ * @param key
+ * @returns {*}
+ */
+TLStorage.getItem = function (key) {
+  let now = Date.now() // 读取时间
+  let wrapItem = localStorage.getItem(key)
+  if (wrapItem) {
+    wrapItem = JSON.parse(wrapItem)
+  }
+  // 读取时间与过期时间进行比较，如果过期则返回null，否则返回value
+  if (parseInt(wrapItem.expireTime) <= now) {
+    TLStorage.removeItem(key)
+    return null
+  } else {
+    return wrapItem.value
+  }
+}
+/**
+ * 清除所有数据项
+ */
+TLStorage.clear = function() {
+  localStorage.clear()
+}
+/**
+ * 删除数据项
+ * @param key
+ */
+TLStorage.removeItem = function(key) {
+  localStorage.removeItem(key)
+}
